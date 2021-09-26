af67187a26b0abf822ae2dc880e6d7456a55c04b
tung <thanhgodlike@gmail.com>
2021-09-26 23:32:38 +0700

    home page

---
+++ lib/utils/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static final Color background= Color.fromRGBO(64, 151, 62, 1);
+  static final Color normalIcon= Colors.white;
+  static final Color selectedIcon= Color.fromRGBO(72, 65, 65, 1);
+  static final Color briefBackground= Color.fromRGBO(244, 243, 243, 1);
+  static final Color textBold= Color.fromRGBO(29, 87, 28, 1);
+  static final Color unavailableStar= Color.fromRGBO(178, 176, 176, 1);
+
+}
+class StringConstants{
+  static final String imageDirectory="assets/images/";
+  static final String iconDirectory="assets/icons/";
+}
