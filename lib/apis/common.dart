b7c406c9b66529f1ab8928ae9bc3b2dad9fbfa5e
linhtnl <lamlinh20001@gmail.com>
2021-10-03 19:54:46 +0700

    Update api path

--- lib/apis/common.dart
+++ lib/apis/common.dart
@@ -1,26 +1,31 @@
 //Change every restart the server
-final URL_SERVER ="";
+final URL_SERVER = "http://52.74.12.123:80/";
 
-class AreaApiPath{
+class AreaApiPath {
   // get all areas
-  final String GET_ALL="";
+  static String GET_ALL = "";
   // get a area by id
-  final String GET_BY_ID ="";
-}
-class BikeApiPath{
-  // get all bikes
-  final String GET_ALL="";
-  // get a bike by id
-  final String GET_BY_ID ="";
-}
-class CustomerApiPath{
-  // get all customers
-  final String GET_ALL="";
-}
-class OwnerApiPath{
-  final String GET_ALL="";
-}
-class AdminApiPath{
-  final String GET_ALL="";
+  static String GET_BY_ID = "";
 }
 
+class BikeApiPath {
+  // get all bikes
+  static String GET_ALL = "api/v1.0/bikes";
+  // get a bike by id
+  static String GET_BY_ID = "";
+}
+
+class CustomerApiPath {
+  // get all customers
+  static String GET_ALL = "";
+  // get a customer by phone
+  static String GET_BY_PHONE = "api/v1.0/customers/phone/";
+}
+
+class OwnerApiPath {
+  static String GET_ALL = "";
+}
+
+class AdminApiPath {
+  static String GET_ALL = "";
+}
