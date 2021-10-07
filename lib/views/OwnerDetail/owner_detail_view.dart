9aea55c0b052db3ae0bc6081119f164048992f1d
linhtnl <lamlinh20001@gmail.com>
2021-10-08 03:20:25 +0700

    Update view

--- lib/views/OwnerDetail/owner_detail_view.dart
+++ lib/views/OwnerDetail/owner_detail_view.dart
@@ -1,25 +1,34 @@
+import 'package:chothuexemay_mobile/models/owner_model.dart';
 import 'package:chothuexemay_mobile/utils/constants.dart';
+import 'package:chothuexemay_mobile/view_model/owner_view_model.dart';
 import 'package:chothuexemay_mobile/views/Components/app_bar.dart';
 import 'package:chothuexemay_mobile/views/Components/botton_app_bar.dart';
 import 'package:chothuexemay_mobile/views/OwnerDetail/components/body.dart';
 import 'package:flutter/material.dart';
-class OwnerDetailView extends StatelessWidget{
+import 'package:provider/provider.dart';
+
+class OwnerDetailView extends StatelessWidget {
+  String id;
   String name;
-  OwnerDetailView({required this.name});
+  OwnerDetailView({required this.id, required this.name});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: ColorConstants.background,
-        title: TopAppBarTitle(title: name,),
+        title: TopAppBarTitle(
+          title: name,
+        ),
       ),
-      body: OwnerDetailBody(name: name,rate: 4.5,),
+      body: OwnerDetailBody(
+        id: id,
+        rate: 4.0, //Temporatry
+      ),
       bottomNavigationBar: BottomAppBar(
         color: ColorConstants.background,
         child: BottomBar(selected: ""),
       ),
     );
   }
-
 }
