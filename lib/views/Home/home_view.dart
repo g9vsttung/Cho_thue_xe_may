d71fa74c93b982cb486466be0034b90c813ea84d
linhtnl <lamlinh20001@gmail.com>
2021-10-19 01:57:42 +0700

    ! IMPORTANT: solution to prepare data - load motor type dropdown

--- lib/views/Home/home_view.dart
+++ lib/views/Home/home_view.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:chothuexemay_mobile/models/motor_type_model.dart';
 import 'package:chothuexemay_mobile/utils/constants.dart';
 import 'package:chothuexemay_mobile/view_model/customer_view_model.dart';
+import 'package:chothuexemay_mobile/view_model/motor_type_view_model.dart';
 import 'package:chothuexemay_mobile/views/Components/app_bar_main.dart';
 import 'package:chothuexemay_mobile/views/Components/botton_app_bar.dart';
 import 'package:chothuexemay_mobile/views/Home/components/body.dart';
@@ -10,17 +12,36 @@ import 'package:provider/provider.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
+  Future<List<MotorType>> getData(BuildContext context) async {
+    await Provider.of<MotorTypeViewModel>(context, listen: false).getAll();
+    return Provider.of<MotorTypeViewModel>(context, listen: false).types;
+  }
 
   @override
   Widget build(BuildContext context) {
     Provider.of<CustomerViewModel>(context).getData();
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: ColorConstants.background,
         title: TopAppBarMain(),
       ),
-      body: BodyHome(),
+      body: FutureBuilder(
+        builder: (context, napshot) {
+          if (napshot.connectionState == ConnectionState.done) {
+            if (napshot.hasData) {
+              final List<MotorType> types = napshot.data as List<MotorType>;
+              return BodyHome(types: types);
+            }
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+        future: getData(context),
+      ),
+      //BodyHome(),
       bottomNavigationBar: BottomAppBar(
         color: ColorConstants.background,
         child: BottomBar(selected: "home"),
